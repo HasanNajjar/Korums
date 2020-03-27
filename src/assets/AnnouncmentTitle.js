86015a2f2c043a766066c704d84cbfd80995f238
HasanNajjar <haasaan9@hotmail.com>
2020-03-27 21:51:09 +0300

    Put the visual components into their own folder, Added Announcments Section

---
+++ src/assets/AnnouncmentTitle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function AnnouncmentTitle() {
+    return(
+        <div>
+    <nav class="flex items-center justify-between flex-wrap bg-gray-300 p-1">
+    <div class="flex items-center flex-shrink-0 text-black mr-6">
+      <span class="font-bold text-xl tracking-wider mx-2">Announcments</span>
+    </div>
+    <div class="block lg:hidden">
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+      </div>
+      <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 mr-2 leading-none border rounded text-black
+       border-black hover:border-transparent hover:text-black hover:bg-green-500 mt-4 lg:mt-0">
+      Next </a>
+    </div>
+      </div>
+  </nav>
+  </div>
+  )
+}
